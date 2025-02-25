582c615bf61123a27a123b2447600e253c9dd23d
Strzebinczyk <kar.strzebinczyk@gmail.com>
2025-02-25 14:04:14 +0100

    Add editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,18 @@ class UsersController < ApplicationController
   def user_params
     params.expect(user: [ :username, :email, :password ])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
